10a6b1f15a4397f43f2f690cd0fa85e4d67c19e3
sba22230 <sba22230@student.cct.ie>
2023-05-21 09:14:22 +0100

    check in of up to date sentiment data frame

--- mydashboard/app.py
+++ mydashboard/app.py
@@ -1,25 +1,85 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from pathlib import Path
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from shiny import App, Inputs, Outputs, Session, reactive, render, ui
+from shiny.plotutils import brushed_points, near_points
 
-import pandas
-from shiny import App, render, ui
+twtsent = pd.read_csv(Path(__file__).parent.parent / "Data/TweetInfo.csv")
+
 
 app_ui = ui.page_fluid(
-    ui.output_table("table"),
+    ui.h2("Tweet Sentiment Analysis"),
+    ui.row(
+        ui.column(
+            3,
+            ui.input_select(
+                "time_range",
+                "Select Time Range to forecast",
+                choices=["no forecast", "1 week", "1 month", "3 months"],
+            ),
+        ),
+        ui.column(
+            2,
+            ui.output_text_verbatim("tweetcount"),
+        ),
+        ui.column(
+            2,
+            ui.output_text_verbatim("tweetcountLkrt"),
+        ),
+    ),
+    ui.row(
+        ui.column(
+            6,
+            ui.output_plot("plot1", click=True, dblclick=True, hover=True, brush=True),
+        ),
+    ),
 )
 
 
-def server(input, output, session):
+def server(input: Inputs, output: Outputs, session: Session):
     @output
-    @render.table
-    def table():
-        infile = Path(__file__).parent.parent / "Data/TweetInfo.csv"
-        df = pandas.read_csv(infile)
-        df = df.sample(10)
-        # Use the DataFrame's to_html() function to convert it to an HTML table, and
-        # then wrap with ui.HTML() so Shiny knows to treat it as raw HTML.
-        return df
+    @render.text()
+    def tweetcount():
+        count = len(twtsent["id"])
+        return str("Number of tweets: " + str(count))
+
+    @output
+    @render.text()
+    def tweetcountLkrt():
+        langCounts = twtsent["LikertScale"].value_counts()
+        langCounts = str("Number of tweets by Likert Scale: \n" + str(langCounts))
+        return langCounts
+
+    @output
+    @render.plot(alt="A scatterplot")
+    def plot1():
+        if input.plot_type() == "matplotlib":
+            fig, ax = plt.subplots()
+            plt.title("Good old twtsent")
+            ax.scatter(twtsent["wkofYr"], twtsent["txbSentiment"])
+            return fig
+
+    # @output
+    # @render.text()
+    # def click_info():
+    #     return "click:\n" + json.dumps(input.plot1_click(), indent=2)
+
+    # @output
+    # @render.text()
+    # def dblclick_info():
+    #     return "dblclick:\n" + json.dumps(input.plot1_dblclick(), indent=2)
+
+    # @output
+    # @render.text()
+    # def hover_info():
+    #     return "hover:\n" + json.dumps(input.plot1_hover(), indent=2)
+
+    # @output
+    # @render.text()
+    # def brush_info():
+    #     return "brush:\n" + json.dumps(input.plot1_brush(), indent=2)
 
 
 app = App(app_ui, server, debug=True)
